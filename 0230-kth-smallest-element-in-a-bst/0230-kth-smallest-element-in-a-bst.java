de511c7a0ad82a8d12ead48e0b1057c9751f5469
vtu24978-cell <vtu24978@veltech.edu.in>
2026-02-21 16:14:15 +0530

    Time: 1 ms (40.22%), Space: 46.8 MB (50.25%) - LeetHub

---
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode curr = root;
+
+        while (curr != null || !stack.isEmpty()) {
+            while (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+            curr = stack.pop();
+            k--;
+
+            if (k == 0) {
+                return curr.val;
+            }
+            curr = curr.right;
+        }
+
+        return -1; 
+    }
+}
