88fe764cffaad9b386613a102def5f939f6731fd
vtu24978-cell <vtu24978@veltech.edu.in>
2026-03-01 09:41:43 +0530

    Time: 5 ms (89.94%), Space: 47.8 MB (36.6%) - LeetHub

---
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minPartitions(String n) {
+        int maxDigit = 0;
+
+        for (char c : n.toCharArray()) {
+            maxDigit = Math.max(maxDigit, c - '0');
+        }
+
+        return maxDigit;
+    }
+}
