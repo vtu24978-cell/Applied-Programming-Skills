cf815f34d23affa5e5d7e13f9b5aaa732e55faa1
vtu24978-cell <vtu24978@veltech.edu.in>
2026-04-15 10:47:22 +0530

    Time: 20 ms (31.62%), Space: 98 MB (70.42%) - LeetHub

---
+++ 0077-combinations/0077-combinations.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(result, new ArrayList<>(), n, k, 1);
+        return result;
+    }
+
+    private void backtrack(List<List<Integer>> result, List<Integer> temp, int n, int k, int start) {
+        if (temp.size() == k) {
+            result.add(new ArrayList<>(temp));
+            return;
+        }
+
+        for (int i = start; i <= n; i++) {
+            temp.add(i);                        // choose
+            backtrack(result, temp, n, k, i + 1); // explore
+            temp.remove(temp.size() - 1);       // un-choose
+        }
+    }
+}
