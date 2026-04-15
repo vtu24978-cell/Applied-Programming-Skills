3dcbcafa0d628b433f85f8c328552e9a976adef1
vtu24978-cell <vtu24978@veltech.edu.in>
2026-04-15 09:55:55 +0530

    Time: 3 ms (73.29%), Space: 53.7 MB (55.21%) - LeetHub

---
+++ 0997-find-the-town-judge/0997-find-the-town-judge.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findJudge(int n, int[][] trust) {
+        int[] score = new int[n + 1];
+
+        for (int[] t : trust) {
+            int a = t[0];
+            int b = t[1];
+
+            score[a]--; // a trusts someone
+            score[b]++; // b is trusted
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (score[i] == n - 1) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
