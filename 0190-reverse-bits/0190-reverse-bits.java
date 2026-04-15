f2c61f6ed93677f0cab42523a5fc2d345182b7e7
vtu24978-cell <vtu24978@veltech.edu.in>
2026-04-15 10:32:09 +0530

    Time: 1 ms (62.62%), Space: 42.6 MB (45.67%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -3,9 +3,10 @@ class Solution {
         int result = 0;
 
         for (int i = 0; i < 32; i++) {
-            result <<= 1;          
-            result |= (n & 1);     
-            n >>>= 1;             
+            int lastBit = n & 1;      // extract last bit
+            result = result << 1;     // shift result
+            result = result | lastBit; // add bit
+            n = n >> 1;              // shift input
         }
 
         return result;
