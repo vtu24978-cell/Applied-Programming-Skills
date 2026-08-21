533c373becf1c2de738af374e077c62afce6230f
vtu24978-cell <vtu24978@veltech.edu.in>
2026-08-21 11:49:58 +0530

    Time: 2 ms (43.46%), Space: 94.6 MB (15.63%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,20 +1,13 @@
-import java.util.Arrays;
 class Solution {
-public int maxProfit(int[] prices) {
-int buy_price = prices[0];
-int profit = 0;
-for (int i = 1; i < prices.length; i++) {
-if (prices[i] < buy_price) {
-buy_price = prices[i];
-} else {
-profit = Math.max(profit, prices[i] - buy_price);
-}
-}
-return profit;
-}
-public static void main(String[] args) {
-Solution s = new Solution();
-int[] prices = {7, 1, 5, 3, 6, 4};
-System.out.println(s.maxProfit(prices));
-}
+    public int maxProfit(int[] prices) {
+        int n= prices.length;
+        int profit=0;
+        int low =prices[0];
+        for(int i=1;i<n;i++){
+                int temp = prices[i]-low;
+                profit =Math.max(profit,temp);
+                low = Math.min(low,prices[i]);
+            }
+            return profit;
+    }
 }
